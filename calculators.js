266827b0d3a1ce4bee8e5fb913de54449f1b5939
Ilona <contact@ilonaferenc.co.uk>
2016-09-15 21:15:48 +0100

    Update calculators.js

--- calculators.js
+++ calculators.js
@@ -1,6 +1,5 @@
 $( document ).ready(function() {
 	
-	
 	/* -----------------------------------------
 	Loans Calculators
 	----------------------------------------- */
@@ -11,21 +10,21 @@ $( document ).ready(function() {
 	
 	  // format number
 	  $(this).val(function(index, value) {
-		return value
-		.replace(/\D/g, "")
-		.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-		;
-	  });
+			return value
+			.replace(/\D/g, "")
+			.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+			;
+		});
 	});
 	
 	function PMT( rate, nper, pv ){
 		/*  	rate	= interest rate for the loan
-			nper	= total number of payments for the loan
-			pv	= present value; principal
-			fv	= future value
-			type	=  when the payments are due:
-					0: end of period
-					1: beginning of period
+					nper	= total number of payments for the loan
+					pv	= present value; principal
+					fv	= future value
+					type	=  when the payments are due:
+							0: end of period
+							1: beginning of period
 		*/
 		
 		var paymentAmount = rate * ( pv * Math.pow((1 + rate), nper) ) / (1 - Math.pow((1 + rate), nper));
@@ -38,11 +37,11 @@ $( document ).ready(function() {
 		var loanAmount 			= parseInt( $( 'input[name="loan-amount"]' ).val().replace(/,/g, '') );
 		var interestRate	 	= parseInt( $( 'input[name="interest-rate"]' ).val().replace(/,/g, '') );
 		var interestOnly		= parseInt( $( '#mortgage select[name="interest-only"]' ).val() );
-		var loanTerm 			= parseInt( $( 'input[name="loan-term"]' ).val() );
-		var results			= $( '.results' );
+		var loanTerm	 			= parseInt( $( 'input[name="loan-term"]' ).val() );
+		var results					= $( '.results' );
 				
-    		var payment 			= ( loanAmount * ( interestRate / 100 ) / 12 );
-		var repayment			= PMT( (interestRate/100)/12, loanTerm*12, -loanAmount );
+    var payment 				= ( loanAmount * ( interestRate / 100 ) / 12 );
+		var repayment				= PMT( (interestRate/100)/12, loanTerm*12, -loanAmount );
 		
 		if( isNaN(loanAmount) || isNaN(interestRate) || isNaN(loanTerm) ){
 			results.html( 'Please fill in all fields.' );	
@@ -57,8 +56,8 @@ $( document ).ready(function() {
 			}
 			
 			output = output + '</div></div>';
-				results.html( output );
+			results.html( output );
 		}
-    });
+  });
 
 });
